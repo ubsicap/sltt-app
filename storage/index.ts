9bca2c92300d8e562729edd1b91a5d72d0973320
Eric Pyle <epyle@biblesocieties.org>
2024-06-21 17:22:25 -0500

    handle testWriteFile with path, seqNum, arrayBuffer

--- storage/index.ts
+++ storage/index.ts
@@ -1,6 +1,6 @@
 import { ipcMain, app } from 'electron'
 import { writeFileSync, mkdirSync } from 'fs'
-import { join } from 'path'
+import { basename, dirname, join } from 'path'
 
 ipcMain.handle('testWriteFile', async (_, arg) => {
     return new Promise(function (resolve, reject) {
@@ -10,6 +10,17 @@ ipcMain.handle('testWriteFile', async (_, arg) => {
             mkdirSync(videosFolder, { recursive: true })
             writeFileSync(join(videosFolder, 'mytest.txt'), 'test worked!')
             resolve('test worked!')
+        } else if (Array.isArray(arg)) {
+            const [path, seqNum, arrayBuffer] = arg
+            const videosFolder = join(app.getPath('userData'), 'fullStorage', 'VideoCache')
+            const relativeVideoPath = dirname(path)
+            const fileName = `${basename(path)}-${seqNum}`
+            const fullFolder = join(videosFolder, relativeVideoPath)
+            const fullPath = join(fullFolder, fileName)
+            mkdirSync(fullFolder, { recursive: true })
+            const buffer = Buffer.from(arrayBuffer)
+            writeFileSync(fullPath, buffer)
+            resolve({ path, seqNum, videosFolder, relativeVideoPath, fileName, fullPath, bufferLength: buffer.length })
         } else {
             reject('this did NOT work!')
         }
