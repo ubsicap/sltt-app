14a1f092abaf288357451529da5e09bd28249295
Eric Pyle <epyle@biblesocieties.org>
2024-06-21 16:20:31 -0500

    begin writing file to videos folder

--- storage/index.ts
+++ storage/index.ts
@@ -1,9 +1,14 @@
-import { ipcMain } from 'electron'
+import { ipcMain, app } from 'electron'
+import { writeFileSync, mkdirSync } from 'fs'
+import { join } from 'path'
 
-ipcMain.handle('myfunc', async (_, arg) => {
+ipcMain.handle('testWriteFile', async (_, arg) => {
     return new Promise(function (resolve, reject) {
         // do stuff
         if (arg === 'test') {
+            const videosFolder = join(app.getPath('userData'), 'fullStorage', 'VideoCache')
+            mkdirSync(videosFolder, { recursive: true })
+            writeFileSync(join(videosFolder, 'mytest.txt'), 'test worked!')
             resolve('test worked!')
         } else {
             reject('this did NOT work!')
