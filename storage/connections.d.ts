4c8f902cc87711b920f13d52c86b688ffebf957f
Eric Pyle <epyle@biblesocieties.org>
2024-10-30 16:55:43 -0500

    begin connections api

--- storage/connections.d.ts
+++ storage/connections.d.ts
@@ -1,9 +1,22 @@
 
 export const CONNECTIONS_API_PROBE = 'probeConnections'
 export const CONNECTIONS_API_CONNECT_TO_URL = 'connectToUrl'
+// not sure if these belong to a different module or not
+export const CONNECTIONS_API_ADD_STORAGE_PROJECT = 'addStorageProject'
+export const CONNECTIONS_API_REMOVE_STORAGE_PROJECT = 'removeStorageProject'
+export const CONNECTIONS_API_GET_STORAGE_PROJECTS = 'getStorageProjects'
+
+export type GetStorageProjectsArgs = { clientId: string, url: string }
+export type GetStorageProjectsResponse = string[]
+
+export type AddStorageProjectArgs = { clientId: string, url: string, project: string, adminEmail: string }
+export type AddStorageProjectResponse = void
+
+export type RemoveStorageProjectArgs = { clientId: string, url: string, project: string, adminEmail: string }
+export type RemoveStorageProjectArgs = void
 
 export type ProbeConnectionsArgs = { clientId: string, urls?: string[] }
 export type ProbeConnectionsResponse = { url: string, accessible: boolean }[]
 
-export type ConnectToUrlArgs = { clientId: string, url: string }
+export type ConnectToUrlArgs = { clientId: string, url: string, project: string }
 export type ConnectToUrlResponse = string
--- storage/connections.ts
+++ storage/connections.ts
@@ -1,7 +1,53 @@
-import { access } from 'fs/promises'
+import { access, appendFile, readFile } from 'fs/promises'
 import { constants, ensureDir } from 'fs-extra'
 import { pathToFileURL, fileURLToPath } from 'url'
-import { ConnectToUrlArgs, ConnectToUrlResponse, ProbeConnectionsArgs, ProbeConnectionsResponse } from './connections.d'
+import { AddStorageProjectArgs, ConnectToUrlArgs, ConnectToUrlResponse, GetStorageProjectsArgs, GetStorageProjectsResponse, ProbeConnectionsArgs, ProbeConnectionsResponse, RemoveStorageProjectArgs } from './connections.d'
+
+export const handleGetStorageProjects = async (defaultStoragePath: string, { clientId }: GetStorageProjectsArgs): Promise<GetStorageProjectsResponse> => {
+    await ensureDir(defaultStoragePath)
+    console.log(`handleGetStorageProjects by client '${clientId}'`)
+    const whitelistPath = `${defaultStoragePath}/whitelist.sltt-projects`
+    const projectsRemoved = new Set<string>()
+    const projectsAdded = new Set<string>()
+    // `whitelist.sltt-projects` file has the following tsv format {timestamp}\t{-|+}\t{project}\t{adminEmail}
+    try {
+        const whitelistContent = await readFile(whitelistPath, 'utf-8')
+        whitelistContent.split('\n').reverse().forEach((line) => {
+            const [_timestamp, action, project, _adminEmail] = line.split('\t')
+            if (action === '+' && !projectsRemoved.has(project)) {
+                projectsAdded.add(project)
+            } else if (action === '-' && !projectsAdded.has(project)) {
+                projectsRemoved.add(project)
+            }
+        })
+    } catch (error) {
+        console.error(`readFile(${whitelistPath}) error`, error)
+    }
+    console.log(`handleGetStorageProjects[${clientId}]: projects added: ${[...projectsAdded]}, projects removed: ${[...projectsRemoved]}`)
+    return [...projectsAdded]
+}
+
+export const handleAddStorageProject = async (defaultStoragePath: string, { clientId, url, project, adminEmail }: AddStorageProjectArgs): Promise<void> => {
+    await ensureDir(defaultStoragePath)
+    console.log(`handleAddStorageProject[${url}]: project '${project}' added by '${adminEmail}' (client '${clientId}')`)
+    try {
+        await appendFile(`${defaultStoragePath}/whitelist.sltt-projects`, `${Date.now()}\t+\t${project}\t${adminEmail}\n`)
+    } catch (error) {
+        console.error(`appendFile(${defaultStoragePath}/whitelist.sltt-projects) error`, error)
+        throw error
+    }
+}
+
+export const handleRemoveStorageProject = async (defaultStoragePath: string, { url, project, adminEmail }: RemoveStorageProjectArgs): Promise<void> => {
+    await ensureDir(defaultStoragePath)
+    console.log(`handleRemoveStorageProject[${url}]: project ${project} removed by ${adminEmail}`)
+    try {
+        await appendFile(`${defaultStoragePath}/whitelist.sltt-projects`, `${Date.now()}\t-\t${project}\t${adminEmail}\n`)
+    } catch (error) {
+        console.error(`appendFile(${defaultStoragePath}/whitelist.sltt-projects) error`, error)
+        throw error
+    }
+}
 
 export const handleProbeConnections = async (defaultStoragePath: string, { urls }: ProbeConnectionsArgs): Promise<ProbeConnectionsResponse> => {
 
