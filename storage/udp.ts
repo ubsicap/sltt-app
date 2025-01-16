76b4cb125a1e7582ea0be289c8ce1e276a174393
Eric Pyle <epyle@biblesocieties.org>
2025-01-16 13:05:07 -0600

    trim udp to client only

--- storage/udp.ts
+++ storage/udp.ts
@@ -1,60 +1,51 @@
 import dgram from 'dgram'
-import os from 'os'
+import { hostname } from 'os'
 import axios from 'axios'
 
-const UDP_SERVER_PORT = 41234
-const UDP_CLIENT_PORT = 41235
+const UDP_CLIENT_PORT = 41234
 
-const SERVER_MSG_SLTT_STORAGE_SERVER_URL = 'SLTT_STORAGE_SERVER_URL:'
+const CLIENT_MSG_SLTT_STORAGE_SERVER_URL = 'SLTT_STORAGE_SERVER_URL:'
 const CLIENT_MSG_HELLO = 'Hello?'
 const CLIENT_MSG_GET_SLTT_STORAGE_SERVER_URL = 'GET /storage-server/url'
 
-// Function to get the local IP address
-const getLocalIPAddress = (): string => {
-    const interfaces = os.networkInterfaces()
-    for (const name of Object.keys(interfaces)) {
-        for (const iface of interfaces[name]!) {
-            if (iface.family === 'IPv4' && !iface.internal) {
-                return iface.address
-            }
-        }
-    }
-    return '127.0.0.1'
-}
-
-const udpServer = dgram.createSocket('udp4')
-
-udpServer.on('listening', () => {
-    const address = udpServer.address()
-    console.log(`Server listening on ${address.address}:${address.port}`)
-})
-
-udpServer.on('message', (msg, rinfo) => {
-    console.log(`Server got: ${msg} from ${rinfo.address}:${rinfo.port}`)
-    if (msg.toString() === CLIENT_MSG_HELLO) {
-        const response = Buffer.from('Hello from server')
-        udpServer.send(response, 0, response.length, rinfo.port, rinfo.address, (err) => {
-            if (err) console.error(err)
-            else console.log('Response sent')
-        })
-    }
-    if (msg.toString() === CLIENT_MSG_GET_SLTT_STORAGE_SERVER_URL) {
-        const response = Buffer.from(`${SERVER_MSG_SLTT_STORAGE_SERVER_URL}${getLocalIPAddress()}:${storageServerPort}`)
-        udpServer.send(response, 0, response.length, rinfo.port, rinfo.address, (err) => {
-            if (err) console.error(err)
-            else console.log('Response sent')
-        })
-    }
-})
-
-udpServer.bind(UDP_SERVER_PORT)
+// unlikely that two clients on the same networ will start at the same time
+const startedAt = new Date().toISOString()
+const myComputerName = hostname()
+let myLocalIpAddress = ''
 
 const udpClient = dgram.createSocket('udp4')
 
 udpClient.on('message', async (msg, rinfo) => {
-    console.log(`Client got: ${msg} from ${rinfo.address}:${rinfo.port}`)
-    const message = msg.toString()
-    if (message.startsWith(SERVER_MSG_SLTT_STORAGE_SERVER_URL)) {
+    const clientData: ClientMessage = JSON.parse(msg.toString())
+    if (clientData.client.computerName === myComputerName && 
+        clientData.client.startedAt === startedAt
+    ) {
+        if (!myLocalIpAddress) {
+            myLocalIpAddress = rinfo.address
+            console.log('My local IP address:', myLocalIpAddress)
+        }
+        console.log('Ignoring own message:', JSON.stringify(clientData, null, 2))
+        return
+    }
+    console.log(`Client got: "${msg}" from '${rinfo.address}:${rinfo.port}'`)
+    const { message } = clientData
+    if (message === CLIENT_MSG_HELLO) {
+        const responseHello = formatClientMsg('Hello')
+        udpClient.send(responseHello, 0, responseHello.length, rinfo.port, rinfo.address, (err) => {
+            if (err) console.error(err)
+            else console.log('Response sent')
+        })
+        return
+    }
+    if (message === CLIENT_MSG_GET_SLTT_STORAGE_SERVER_URL) {
+
+        const responseSlttStorageServerUrl = formatClientMsg(`${CLIENT_MSG_SLTT_STORAGE_SERVER_URL}${myLocalIpAddress}:${storageServerPort}`)
+        udpClient.send(responseSlttStorageServerUrl, 0, responseSlttStorageServerUrl.length, rinfo.port, rinfo.address, (err) => {
+            if (err) console.error(err)
+            else console.log('Response sent')
+        })
+    }
+    if (message.startsWith(CLIENT_MSG_SLTT_STORAGE_SERVER_URL)) {
         const [, ip, port] = message.split(':')
         const serverUrl = `http://${ip}:${port}`
         console.log(`Discovered storage server at ${serverUrl}`)
@@ -71,12 +62,28 @@ udpClient.on('message', async (msg, rinfo) => {
     }
 })
 
+type ClientMessage = {
+    client: {
+        startedAt: string,
+        computerName: string,
+    },
+    message: string,
+}
+
+const formatClientMsg = (msg: string): Buffer => {
+    const payload: ClientMessage = {
+        client: { startedAt, computerName: myComputerName },
+        message: msg,
+    }
+    return Buffer.from(JSON.stringify(payload))
+}
+
 udpClient.on('listening', () => {
     const address = udpClient.address()
     console.log(`Client listening on ${address.address}:${address.port}`)
-    const message = Buffer.from(CLIENT_MSG_HELLO)
+    const msgHello = formatClientMsg(CLIENT_MSG_HELLO)
     udpClient.setBroadcast(true)
-    udpClient.send(message, 0, message.length, UDP_SERVER_PORT, '255.255.255.255', (err) => {
+    udpClient.send(msgHello, 0, msgHello.length, UDP_CLIENT_PORT, '255.255.255.255', (err) => {
         if (err) console.error(err)
         else console.log('Broadcast message sent')
     })
@@ -88,8 +95,8 @@ let storageServerPort = NaN
 
 export const setupUDPServer = (port: number): void => {
     storageServerPort = port
-    const message = Buffer.from(CLIENT_MSG_GET_SLTT_STORAGE_SERVER_URL)
-    udpClient.send(message, 0, message.length, UDP_SERVER_PORT, '255.255.255.255', (err) => {
+    const msgGetStorageServerUrl = formatClientMsg(CLIENT_MSG_GET_SLTT_STORAGE_SERVER_URL)
+    udpClient.send(msgGetStorageServerUrl, 0, msgGetStorageServerUrl.length, UDP_CLIENT_PORT, '255.255.255.255', (err) => {
         if (err) console.error(err)
         else console.log('Broadcast message sent')
     })
